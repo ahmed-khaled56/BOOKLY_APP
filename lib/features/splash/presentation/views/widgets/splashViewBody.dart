d86beb6e47b8ff55b769cc444bf577b7d5df8685
Ahmed Khaled <mhmamdmf275@gmail.com>
2025-09-11 16:32:38 +0300

    add image to ui

--- lib/features/splash/presentation/views/widgets/splashViewBody.dart
+++ lib/features/splash/presentation/views/widgets/splashViewBody.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utlis/assets.dart';
 import 'package:flutter/material.dart';
 
 class Splashviewbody extends StatelessWidget {
@@ -5,6 +6,10 @@ class Splashviewbody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: []);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [Image.asset(AssetsData.logo)],
+    );
   }
 }
