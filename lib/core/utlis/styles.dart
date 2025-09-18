6392d8ae1dc633b538e82d1f6da1732e7b89919a
Ahmed Khaled <mhmamdmf275@gmail.com>
2025-09-18 15:47:00 +0300

    applay  kGtSectraFine, text

--- lib/core/utlis/styles.dart
+++ lib/core/utlis/styles.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
@@ -7,12 +8,14 @@ abstract class Styles {
     fontWeight: FontWeight.w500,
   );
   static const Bookname = TextStyle(
+    fontFamily: kGtSectraFine,
     color: Colors.white,
     fontSize: 20,
     fontWeight: FontWeight.bold,
   );
 
   static const AutherName = TextStyle(
+    fontFamily: kGtSectraFine,
     color: Colors.white,
     fontSize: 12,
 
@@ -20,15 +23,21 @@ abstract class Styles {
   );
 
   static const priceStyle = TextStyle(
+    fontFamily: kGtSectraFine,
     color: Colors.white,
     fontSize: 20,
 
     fontWeight: FontWeight.bold,
   );
 
-  static const downloadNumber = TextStyle(color: Colors.white, fontSize: 12);
+  static const downloadNumber = TextStyle(
+    fontFamily: kGtSectraFine,
+    color: Colors.white,
+    fontSize: 15,
+  );
 
   static const rate = TextStyle(
+    fontFamily: kGtSectraFine,
     color: Colors.white,
     fontSize: 15,
 
