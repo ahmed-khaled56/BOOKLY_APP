a21ec771945e3507fd4833dc94736ea6973cfad8
Ahmed Khaled <mhmamdmf275@gmail.com>
2025-09-30 18:10:30 +0300

    create featch featured books in cubit

--- lib/features/home/presentation/manager/featured_cubit_cubit.dart
+++ lib/features/home/presentation/manager/featured_cubit_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_cubit_state.dart';
 
 class FeaturedCubitCubit extends Cubit<FeaturedState> {
-  FeaturedCubitCubit() : super(FeaturedInitialState());
+  FeaturedCubitCubit(this.homeRepo) : super(FeaturedInitialState());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturerdBooks() async {
+    emit(FeaturedLoadState());
+
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+      (failur) {
+        emit(FeaturedFailurState(erMessage: failur.errorMessage));
+      },
+      (books) {
+        emit(FeaturedSucessState(books: books));
+      },
+    );
+  }
 }
